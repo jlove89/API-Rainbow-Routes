1223925247ff43db22e35edae907d656831a2793
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-06-18 14:32:38 -0400

    install json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
+"use strict";
 // Get the environment variables
-require('dotenv').config()
-
+require('dotenv').config();
 // Require needed node modules
-const express = require('express')
-
+const express = require('express');
 // Initialize your application by calling the function returned by the express module
-const app = express()
-
+const app = express();
 // Declare routes that people can visit on the application
-
 // White Page
 app.get('/', function (req, res) {
     res.send(`
@@ -18,12 +15,11 @@ app.get('/', function (req, res) {
             </div>
             <h1>White Page</h1>
         </body>
-    `)
-})
-
+    `);
+});
 // Color Page
 app.get('/:color', function (req, res) {
-    let myColor = req.params.color
+    let myColor = req.params.color;
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
@@ -31,10 +27,7 @@ app.get('/:color', function (req, res) {
             </div>
             <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
         </body>
-    `)
-})
-
-
+    `);
+});
 // Listen to a port number defined by a local environment variable
-app.listen(process.env.PORT)
-// comment
+app.listen(process.env.PORT);
